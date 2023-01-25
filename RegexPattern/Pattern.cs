92bfa6bd595834e507445f077b379de9da99ab0a
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-25 21:30:40 +0530

    optional 2 character country code after co(dot)

--- RegexPattern/Pattern.cs
+++ RegexPattern/Pattern.cs
@@ -15,7 +15,8 @@ namespace RegexPattern
             // Part3: ensure dot after bridgelabz and validate mandatory part co
 
             // Part4: optional part xyz, validate special characters _,+,-,. proceeding to xyz
-            var regex = new Regex(@"^([a][b][c])([_\.\+\-])([a-zA-Z0-9]+)\@([b][r][i][d][g][e][l][a][b][z])\.([c][o])*$");
+            // Part5: dot and optional country code and its 2 characters only after co
+            var regex = new Regex(@"^([a][b][c])([_\.\+\-])([a-zA-Z0-9]+)\@([b][r][i][d][g][e][l][a][b][z])\.([c][o])\.([a-z]{2})*$");
             bool matchRes = regex.IsMatch(eMail);
             if (matchRes == true)
             {
--- RegexPattern/Program.cs
+++ RegexPattern/Program.cs
@@ -6,8 +6,13 @@
         {
             Console.WriteLine("Welcome to Regular Expressions");
 
+            Console.Write("Enter an EMail Address: ");
+            Console.Write("(Format for EMail Address ==> abc.xyz@bridgelabz.co.in, where abc, bridgelabz and co are mandatory): ");
+            string userInput = Console.ReadLine();
+
             Pattern objPattern = new Pattern();
-            objPattern.ValidateEMail("abc.xyz@bridgelabz.co");
+            objPattern.ValidateEMail(userInput);
+            Console.ReadLine();
         }
     }
 }
