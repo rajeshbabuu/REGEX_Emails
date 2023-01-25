93dc41d14007b06fb36f9aecd6fcf3664aae6c85
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-25 21:16:29 +0530

    abc is mandatory Part in email

---
+++ RegexPattern/Pattern.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPattern
+{
+    public class Pattern
+    {
+        public void ValidateEMail(string eMail)
+        {
+            // abc is mandatory
+            var regex = new Regex(@"^([a][b][c])");
+            bool matchRes = regex.IsMatch(eMail);
+            if (matchRes == true)
+            {
+                Console.WriteLine($"\n{eMail} is valid email address");
+            }
+            else
+            {
+                Console.WriteLine($"\n{eMail} is invalid email address");
+            }
+        }
+    }
+}
---
+++ RegexPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace RegexPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regular Expressions");
+
+            Pattern objPattern = new Pattern();
+            objPattern.ValidateEMail("abc");
+        }
+    }
+}
